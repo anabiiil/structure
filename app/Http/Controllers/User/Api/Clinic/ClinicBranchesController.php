1a985d90b00ff1270f6d624da46604029e5ac040
Abd El-Rahman Nabil <a.nabiil@hotmail.com>
2025-08-17 04:51:09 +0300

    add model and migrations files

---
+++ app/Http/Controllers/User/Api/Clinic/ClinicBranchesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\User\Api\Clinic;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClinicBranchesController extends Controller
+{
+    //
+}
+
